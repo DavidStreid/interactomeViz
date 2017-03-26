7c3815925d6c9f85e91da7e47e4e2f2606c7b4d0
David Streid <dcs2153@columbia.edu>
2017-03-25 17:03:49 -0700

    fixed makeDAG script

--- makeDAG.py
+++ makeDAG.py
@@ -4,23 +4,38 @@ import re
 import json
 import requests
 
-filename = 'HI-I-05.tsv'
+'''
+INPUT:
+    Entrez Gene IDA Symbol A    Entrez Gene IDB Symbol B
+    60  ACTB    60  ACTB
+    71  ACTG1   60  ACTB
+    71  ACTG1   71  ACTG1
+    95  ACY1    95  ACY1
+
+OUTPUT:
+    export var nodes = [{id: '#',label: 'entrezID'}, ...];
+    export var links = [{source: '#', target: '#'}, ...];
+'''
+
+if(len(sys.argv) < 3):
+    print "ERROR: Specify a file and min number of links per node"
+    sys.exit()
+
+filename = sys.argv[1]  # Data containing interactome data
+minLinks = int(sys.argv[2])  # Defines the number of links each node should have
 
 with open(filename,'r') as i:
     lines = i.readlines()
     data = lines[1:]            # Remove header - ['Entrez Gene IDA', 'Symbol A', 'Entrez Gene IDB', 'Symbol B\n']
-    count = 1
     if(data):                   # If file contains non-header information
         linkNumDic = {}         # Keeps track of the number of links each node has
         nodeDic = {}            # Keeps track of nodes
         idToEntrezID = {}       # Mapping of id to entrezID
         links = []              # links
         idCount = 1             # id count for graph
-        numNodes = 100
+        count = 1
 
-
-
-        # Get Nodes
+        # Get Number of links in nodes
         for line in data:
             lineSep = line.split('\t')  # ['541468', 'LOC541468', '164127', 'FLJ35728\n']
             entrezID_A = lineSep[0]
@@ -33,13 +48,22 @@ with open(filename,'r') as i:
             else:
                 linkNumDic[entrezID_A] += 1
 
-            if(idCount < numNodes):
+        # Get Node information for genes with min number of links
+        for line in data:
+            lineSep = line.split('\t')  # ['541468', 'LOC541468', '164127', 'FLJ35728\n']
+            entrezID_A = lineSep[0]
+            symbol_A = lineSep[1]
+            entrezID_B = lineSep[2]
+            symbol_B = lineSep[3]
+
+            if(linkNumDic[entrezID_A] > minLinks):
                 # Add node if it doesn't exist
                 if entrezID_A not in nodeDic:
-                    nodeDic[entrezID_A] = json.dumps({"id": str(idCount), "label": entrezID_A}, sort_keys=False)
+                    nodeDic[entrezID_A] = json.dumps({"id": str(idCount), "label": symbol_A}, sort_keys=False)
                     idToEntrezID[entrezID_A] = str(idCount)
                     idCount += 1
 
+        # Get links for nodes with min number of links
         for line in data: 
             lineSep = line.split('\t')  # ['541468', 'LOC541468', '164127', 'FLJ35728\n']
             entrezID_A = lineSep[0]
@@ -53,6 +77,8 @@ with open(filename,'r') as i:
                     # Add link
                     links.append(json.dumps({"source": idToEntrezID[entrezID_A], "target": idToEntrezID[entrezID_B]}, sort_keys=False))      
 
+    print "Nodes: " + str(len(nodeDic.keys()))
+    print "Links: " + str(len(links))
     nodeFile = "node.txt"
     linkFile = "links.txt"
 
@@ -70,48 +96,3 @@ with open(filename,'r') as i:
     for link in links:
         target.write(link + ',')
     target.write('\n]')
-
-#export var nodes = [{
-#     id: 'start',
-#     label: 'start'
-#   }, {
-#     id: '1',
-#     label: 'Query ThreatConnect',
-#     rank: 'first'
-#   }, {
-#     id: '2',
-#     label: 'Query XForce',
-#     rank: 'first'
-#   }, {
-#     id: '3',
-#     label: 'Format Results'
-#   }, {
-#     id: '4',
-#     label: 'Search Splunk'
-#   }, {
-#     id: '5',
-#     label: 'Block LDAP'
-#   }, {
-#     id: '6',
-#     label: 'Email Results'
-#   }];
-
-# export var links = [{
-#     source: 'start',
-#     target: '1'
-#   }, {
-#     source: 'start',
-#     target: '2'
-#   }, {
-#     source: '1',
-#     target: '3'
-#   }, {
-#     source: '2',
-#     target: '4'
-#   }, {
-#     source: '2',
-#     target: '6'
-#   }, {
-#     source: '3',
-#     target: '5'
-#   }];
